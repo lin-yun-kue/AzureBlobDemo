20b83a4844f24afa4d3f1f0037ec50454d2081f4
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2019-01-24 16:20:56 +0800

    blob storage

---
+++ BlobStorage/Program.cs
@@ -0,0 +1,50 @@
+﻿using BlobStorage.helper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectString = ConfigurationManager.AppSettings["AzureStorage"];
+            var blobHelper = new BlobHelper(connectString);
+
+            //upload file
+            //var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            //var filePath = $"{basePath}image\\pic1.png";
+            //var filePath = $"{basePath}image\\pic2.png";
+            //using (var fileStream = System.IO.File.OpenRead(filePath))
+            //{
+            //    blobHelper.UploadFile("kyletest", "test.png", fileStream);
+            //}
+
+            //-----------------
+            //delete file
+            //blobHelper.DeleteFile("kyletest", "test.png");
+
+            //upload large file
+            //var sw = new Stopwatch();
+            //sw.Reset();
+            //sw.Start();
+
+            //var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            //var filePath = $"{basePath}video\\video1.mp4";
+            //using (var fileStream = System.IO.File.OpenRead(filePath))
+            //{
+            //    //blobHelper.UploadFile("kyletest", "test_video.mp4", fileStream);
+            //    blobHelper.UploadLargeFile("kyletest", "test_video.mp4", fileStream);
+            //}
+            //sw.Stop();
+            //var result = sw.Elapsed.TotalMilliseconds;
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ BlobStorage/helper/BlobHelper.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobStorage.helper
+{
+    public class BlobHelper
+    {
+        public string StorageConnectionString { get; set; }
+        public string ErrorMessage { get; set; }
+
+        private CloudStorageAccount _storageAccount;
+        private CloudBlobClient _blobClient;
+
+        public BlobHelper(string connectString)
+        {
+            this.StorageConnectionString = connectString;
+
+            _storageAccount = CloudStorageAccount.Parse(this.StorageConnectionString);
+            _blobClient = _storageAccount.CreateCloudBlobClient();
+        }
+
+        public bool UploadFile(string container, string fileName, Stream stream)
+        {
+            bool status = true;
+            try
+            {
+                CloudBlobContainer blobContainer = _blobClient.GetContainerReference(container);
+                blobContainer.CreateIfNotExistsAsync().Wait();
+                CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference(fileName);
+                blockBlob.UploadFromStream(stream);
+            }
+            catch (Exception e)
+            {
+                ErrorMessage = e.Message;
+                status = false;
+            }
+            return status;
+        }
+
+        public bool UploadLargeFile(string container, string fileName, Stream stream)
+        {
+            TimeSpan backOffPeriod = TimeSpan.FromSeconds(2);
+            int retryCount = 1;
+            BlobRequestOptions bro = new BlobRequestOptions()
+            {
+                SingleBlobUploadThresholdInBytes = 50 * 1024 * 1024, 
+                ParallelOperationThreadCount = 5,
+                RetryPolicy = new ExponentialRetry(backOffPeriod, retryCount),
+            };
+            _blobClient.DefaultRequestOptions = bro;
+            CloudBlobContainer blobContainer = _blobClient.GetContainerReference(container);
+
+            CloudBlockBlob blob = blobContainer.GetBlockBlobReference(fileName);
+
+            blob.UploadFromStream(stream);
+
+            return true;
+        }
+
+        public bool DeleteFile(string container, string fileName)
+        {
+            bool status = true;
+            try
+            {
+                CloudBlobContainer blobContainer = _blobClient.GetContainerReference(container);
+                CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference(fileName);
+                blockBlob.DeleteIfExists();
+            }
+            catch (Exception e)
+            {
+                ErrorMessage = e.Message;
+                status = false;
+            }
+            return status;
+        }
+
+        public string GetFileUrl(string container, string fileName)
+        {
+            CloudBlobContainer blobContainer = _blobClient.GetContainerReference(container);
+            CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference(fileName);
+            return (blockBlob.Exists()) ? blockBlob.Uri.AbsoluteUri : "";
+        }
+    }
+}
